29dae4a2e2d9d7488ffeb87f0e44b2c6041e9e4b
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2017-09-25 22:22:05 +0200

    Fixed crashing json serialization for swift 4

--- Mockingjay/Builders.swift
+++ Mockingjay/Builders.swift
@@ -28,7 +28,7 @@ public func http(_ status:Int = 200, headers:[String:String]? = nil, download:Do
 public func json(_ body: Any, status:Int = 200, headers:[String:String]? = nil) -> (_ request: URLRequest) -> Response {
   return { (request:URLRequest) in
     do {
-      let data = try JSONSerialization.data(withJSONObject: body, options: JSONSerialization.WritingOptions())
+      let data = try JSONSerialization.data(withJSONObject: body, options: .prettyPrinted)
       return jsonData(data, status: status, headers: headers)(request)
     } catch {
       return .failure(error as NSError)
